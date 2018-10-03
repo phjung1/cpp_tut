f6db7af7eea146ef83168e5850c7dd0d8190ed37
phjung1 <36128943+phjung1@users.noreply.github.com>
2018-10-04 06:25:01 +0900

    #include ,
    std:cout <<

---
+++ Project1/소스.cpp
@@ -0,0 +1,5 @@
+#include <iostream>	// instructs the compiler to include the declarations of standard strean I/O facilities as found in iostream.
+
+int main() {
+	std::cout << "Hello world!\n";  // << --> put to
+}
