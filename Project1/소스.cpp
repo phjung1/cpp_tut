0fbf90ce697e2d846e60b7a495790a0da0b0ba05
phjung1 <36128943+phjung1@users.noreply.github.com>
2018-10-04 06:29:33 +0900

    void,
    funtion,

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,5 +1,15 @@
-#include <iostream>	// instructs the compiler to include the declarations of standard strean I/O facilities as found in iostream.
+#include <iostream>	
+
+using namespace std; // make names from std visible without std::
+
+double square(double x) { // square a  double precision floating-point number
+	return x * x;
+}
+
+void print_square(double x) { // A "return type" void indicates a function does not return a value.
+	cout << "the square of " << x << " is " << square(x) << "\n";
+}
 
 int main() {
-	std::cout << "Hello world!\n";  // << --> put to
+	print_square(1.234); //print: the suqare of 1.234 is 1.52278
 }
